850a98ac5dfef2c85e2a49d0f01478173ddc7dc0
Nikko S <nikkolas.c.sanchez@gmail.com>
2024-09-28 16:40:32 -0700

    added lsp.lua config and update ui.lua

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -71,6 +71,24 @@ return {
     end,
   },
 
+  -- buffer line
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    -- keys = {
+    -- 	{ "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+    -- 	{ "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+    -- },
+    opts = {
+      options = {
+        mode = "buffers",
+        -- separator_style = "slant",
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+      },
+    },
+  },
+
   -- filename
   {
     "b0o/incline.nvim",
